9f5976b72394318f8d0ed338059ea78754ea2063
SeongYeonYun <datascience123@naver.com>
2021-02-09 13:46:31 +0900

    initial commit

---
+++ venv/slack.py
@@ -0,0 +1,6 @@
+from slacker import Slacker
+
+slack = Slacker('xoxb-1694440446322-1694442730642-rqX0cmQZCJkY06P3CVwnxW5W')
+
+# Send a message to #general channel
+slack.chat.post_message('#stock', 'Hello fellow slackers!')
